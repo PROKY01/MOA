3560b70509f3e3e8196d71beb521b825566128dd
PROKY01 <L20240054@prumyslovkajihlava.cz>
2026-09-17 08:46:09 +0200

    Automaticka zaloha

--- hello_world/index.js
+++ hello_world/index.js
@@ -1,3 +1,21 @@
-function sayHello() {
-    document.getElementById("hello").innerText == "Hello World!";
+if("serviceWorker" in navigator) {
+    window.addEventListener("load", ()=> {
+        navigator.serviceWorker.register("sw.js")
+        .then(registration => {
+            console.log("SW registerd");
+        })
+        .catch(error => {
+            console.log("SW registration failed: ", error);
+        })
+    });
 }
+
+function sayHello(){
+    if(document.getElementById("hello").innerText == "Hello World!"){
+        document.getElementById("hello").innerText = "Ahoj světe!";
+    }
+    else{
+        document.getElementById("hello").innerText = "Hello World!";
+    }
+    }
+    
